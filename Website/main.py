c1a3e8e23ef34011e897148d815a57b70f0d45ed
szymmoc895 <szymon.moc@outlook.com>
2024-06-28 21:02:14 +0200

    wyswietlanie wykresu w html

--- Website/main.py
+++ Website/main.py
@@ -1,8 +1,12 @@
 
-from flask import Flask, get_flashed_messages, render_template, request, url_for, redirect, flash
+from flask import Flask, get_flashed_messages, render_template, request, send_file, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
 import bcrypt
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import io
  
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
@@ -95,7 +99,51 @@ def logout():
 @app.route("/")
 def home():
     return render_template("home.html")
- 
+
+
+@app.route('/generate_plot')
+def generate_plot():
+    # Użycie surowego ciągu znaków (raw string) do ścieżki pliku CSV
+    file_path =  '/home/szym/Documents/Github/WebsiteProject/BAZA/dane.csv'
+
+    # Wczytanie pliku CSV z odpowiednim separatorem
+    data = pd.read_csv(file_path, sep=';')
+
+    # Wyświetlenie kolumn i podgląd danych
+    print("Kolumny w danych:")
+    print(data.columns)
+
+    print("\nPodgląd danych:")
+    print(data.head())
+
+    # Konwersja kolumn do odpowiednich typów
+    data['wartosc'] = pd.to_numeric(data['wartosc'], errors='coerce')
+    data['rok'] = pd.to_numeric(data['rok'], errors='coerce')
+
+    # Usunięcie wierszy z brakującymi danymi
+    data = data.dropna(subset=['wartosc', 'rok'])
+
+    # Filtracja danych tylko dla przystępujących
+    przystepujacy = data[data['status_zdajacych'].str.contains('przystąpił', case=False, na=False)]
+
+    # Główny wykres: średnia liczba osób przystępujących do egzaminu w poszczególnych województwach
+    avg_values = przystepujacy.groupby('wojewodztwo')['wartosc'].mean().reset_index()
+
+    plt.figure(figsize=(15, 7))
+    sns.barplot(data=avg_values, x='wojewodztwo', y='wartosc', palette='viridis')
+    plt.title('Średnia liczba osób przystępujących do egzaminu maturalnego w poszczególnych województwach')
+    plt.xlabel('Województwo')
+    plt.ylabel('Średnia liczba osób')
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plt.close()
+
+    return send_file(img, mimetype='image/png')
+    
  
 if __name__ == "__main__":
     app.run()
